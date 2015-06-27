bcb2630f2304fd814f40e26e85f99d2882aa1cd9
CJ Johnson <Fermentedbeats@gmail.com>
2015-06-26 20:17:38 -0700

    comment render fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
 		redirect_to message_path(@message)
 	end
 
+	def destroy
+		@message = Message.find(params[:message_id])
+		@comment = @message.comments.find(params[:id])
+		@comment.destroy
+		redirect_to message_path(@message)
+	end
+
 	private
 	def comment_params
 		params.require(:comment).permit(:commenter, :body)
